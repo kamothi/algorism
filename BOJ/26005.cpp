aaaf4b9978629a49cc2a44c6502eff45d5340b5f
kwang il <57484942+kamothi@users.noreply.github.com>
2023-01-05 15:00:31 +0900

    Create 26005.cpp

---
+++ BOJ/26005.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n == 1) {
+        cout << 0;
+        return 0;
+    }
+    cout << (n*n/2) + n*n%2;
+}
