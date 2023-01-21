7c970dcdcb4c3ef93407e03e1bcdf334a2f0bf8a
kwang il <57484942+kamothi@users.noreply.github.com>
2023-01-21 19:08:59 +0900

    Create 1912.cpp

---
+++ BOJ/1912.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    int arr[100000];
+    int dp[100000];
+    cin >> n;
+    for(int i=0;i<n;i++) cin >> arr[i];
+    dp[0] = arr[0];
+    int result=0;
+    for(int i=1;i<n;i++){
+        dp[i] = max(arr[i]+dp[i-1],arr[i]);
+        result = max(result,dp[i]);
+    }
+    cout << result;
+}
