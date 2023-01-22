853f8d671384f0fca76501465330bb70f5c41a8a
kwang il <57484942+kamothi@users.noreply.github.com>
2023-01-22 21:03:05 +0900

    Update 1912.cpp

--- BOJ/1912.cpp
+++ BOJ/1912.cpp
@@ -13,7 +13,7 @@ int main(){
     cin >> n;
     for(int i=0;i<n;i++) cin >> arr[i];
     dp[0] = arr[0];
-    int result=0;
+    int result=dp[0];
     for(int i=1;i<n;i++){
         dp[i] = max(arr[i]+dp[i-1],arr[i]);
         result = max(result,dp[i]);
