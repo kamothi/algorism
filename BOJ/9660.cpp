26846d4c688183c1c76c61ad18d9d9810972ec77
kwang il <57484942+kamothi@users.noreply.github.com>
2024-04-01 14:27:09 +0900

    Create 9660.cpp

---
+++ BOJ/9660.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long n;
+    cin >> n;
+
+    if(n%7 == 2 || n%7 == 0) cout << "CY";
+    else cout << "SK";
+}
