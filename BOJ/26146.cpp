1522fbab2df0814183516aa8588be090c6324808
kwang il <57484942+kamothi@users.noreply.github.com>
2024-04-12 23:59:12 +0900

    Update 26146.cpp

--- BOJ/26146.cpp
+++ BOJ/26146.cpp
@@ -5,12 +5,10 @@ using namespace std;
 
 vector<int> v[200001];
 vector<int> v2[200001];
-bool check[200001];
 bool visited[200001];
 
 void dfs(int n){
     visited[n] = 1;
-    check[n] = 1;
 
     for(int i=0;i<v[n].size();i++){
         if(!visited[v[n][i]]) dfs(v[n][i]);
@@ -19,7 +17,6 @@ void dfs(int n){
 
 void dfsv2(int n){
     visited[n] = 1;
-    check[n] = 1;
 
     for(int i=0;i<v2[n].size();i++){
         if(!visited[v2[n][i]]) dfsv2(v2[n][i]);
@@ -44,16 +41,15 @@ int main(){
     bool success = true;
     dfs(1);
     for(int i=1;i<=n;i++) {
-        if(!check[i]) success = 0;
+        if(!visited[i]) success = 0;
         visited[i] = 0;
-        check[i] = 0;
     }
 
     dfsv2(1);
     for(int i=1;i<=n;i++) {
-        if(!check[i]) success = 0;
+        if(!visited[i]) success = 0;
     }
 
-    if(success) cout << "YES";
-    else cout << "NO";
+    if(success) cout << "Yes";
+    else cout << "No";
 }
