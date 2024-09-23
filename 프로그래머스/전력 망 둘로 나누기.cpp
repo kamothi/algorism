1c8707dbc3d7c1420af15f56df1ab2b314cc2e40
kwang il <57484942+kamothi@users.noreply.github.com>
2024-09-23 22:05:24 +0900

    Create 전력 망 둘로 나누기.cpp

---
+++ 프로그래머스/전력 망 둘로 나누기.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int cnt;
+vector<int> tree[101];
+
+void bfs(int v1, int v2){
+    queue<int> q;
+    vector<bool> visited(101);
+    
+    q.push(v1);
+    visited[v1] = true;
+    visited[v2] = true;
+    
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        
+        for (int i=0; i<tree[node].size(); i++) {
+            int node2 = tree[node][i];
+                                                 
+            if (visited[node2]) continue;
+            
+            cnt++;
+            q.push(node2);
+            visited[node2] = true;
+        }
+    }
+}
+
+int solution(int n, vector<vector<int>> wires) {
+    int answer = 101;
+    
+    for (auto wire: wires) {
+        tree[wire[0]].push_back(wire[1]);
+        tree[wire[1]].push_back(wire[0]);
+    }
+    
+    for (auto wire: wires) {
+        cnt = 1;
+        int v1 = wire[0];
+        int v2 = wire[1];
+       
+        bfs(v1, v2);
+        answer = min(answer, abs(2 * cnt - n));
+   }
+    return answer;
+}
